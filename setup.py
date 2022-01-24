5ba1851f0d06a171d43214a9185b9b624ce5a958
DaffyDiamond <70351316+DaffyDiamond@users.noreply.github.com>
2022-01-23 18:08:27 -0600

    setup.py to create executable file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from cx_Freeze import setup, Executable
+
+executables = [Executable("menu.py")]
+
+setup(
+    name="Bamboo Taskview",
+    version="0.3.0",
+    description="Beta Release",
+    executables=executables,
+)
